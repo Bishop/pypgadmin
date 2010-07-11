fe6e92203776ea9e423905de2fc660be79697424
Alexandr Prudnikov <al.prudnikov@gmail.com>
2010-07-11 14:49:27 +0300

    Content-Type for static files; fix for binary files

--- static.py
+++ static.py
@@ -1,6 +1,12 @@
 import os
 
 class StaticFilesFilter(object):
+	content_types = {
+		'css': 'text/css; charset=utf-8',
+		'js': 'text/javascript; charset=utf-8',
+		'png': 'text/plain',
+	}
+
 	def __init__(self, application):
 		self.app = application
 
@@ -9,12 +15,13 @@ class StaticFilesFilter(object):
 		if not os.path.isfile(file_path):
 			return self.app(environ, start_response)
 
-		start_response("200 OK", [])
+
+		start_response("200 OK", [('Content-Type', self.content_types[os.path.splitext(file_path)[1][1:]])])
 
 		return self.send_file(file_path)
 
 	def send_file(self, file_path):
-		with open(file_path) as f:
+		with open(file_path, 'rb') as f:
 			block = f.read(4096)
 			while block:
 				yield block
