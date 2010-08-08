956d5d27862278d02a65991fb6f02c9d80ec301a
Alexandr Prudnikov <al.prudnikov@gmail.com>
2010-08-08 20:56:43 +0300

    Object Application

--- index.py
+++ index.py
@@ -7,17 +7,17 @@ os.curdir = os.path.dirname(__file__)
 import pyPgAdmin
 import static
 
-def application(environ, start_response):
-	if 'PYTHON_EGG_CACHE' in environ:
-		os.environ['PYTHON_EGG_CACHE'] = environ['PYTHON_EGG_CACHE']
-	else:
-		os.environ['PYTHON_EGG_CACHE'] = os.path.join(os.path.dirname(__file__),
-		'.egg-cache')
-
-	return pyPgAdmin.dispatch_request(environ, start_response)
+#def application(environ, start_response):
+#	if 'PYTHON_EGG_CACHE' in environ:
+#		os.environ['PYTHON_EGG_CACHE'] = environ['PYTHON_EGG_CACHE']
+#	else:
+#		os.environ['PYTHON_EGG_CACHE'] = os.path.join(os.path.dirname(__file__),
+#		'.egg-cache')
+#
+#	return pyPgAdmin.dispatch_request(environ, start_response)
 
 def factory():
-	return static.StaticFilesFilter(application)
+	return static.StaticFilesFilter(pyPgAdmin.Application())
 
 if __name__ == '__main__':
 	from  wsgiref.simple_server import make_server
--- pyPgAdmin.py
+++ pyPgAdmin.py
@@ -22,94 +22,105 @@ urls = [
 	(r'^get_dbs/(?P<profile>\w+)', 'get_databases'),
 ]
 
-def get_databases(environ, start_response, profile):
-	c = connection_params(profile)
-	dbm = base.DataBaseManager(**c)
-	start_response('200 OK', [('Content-Type', 'text/html; charset=utf-8')])
-	return json.JSONEncoder().encode(dbm.get_databases())
+class Application(object):
+	environ = None
+	start_response = None
+	def __call__(self, environ, start_response):
+		return self.dispatch_request(environ, start_response)
+
+	def dispatch_request(self, environ, start_response):
+		self.environ = environ
+		self.start_response = start_response
+
+		path = environ.get('PATH_INFO', '').strip('/')
+
+		for regex, callback in urls:
+			match = re.search(regex, path)
+			if match is not None:
+				return getattr(self, callback)(**match.groupdict())
+		else:
+			return not_found(environ, start_response)
+
+	def not_found(self):
+		""" 404 """
+		self.start_response('404 NOT FOUND', [('Content-Type', 'text/plain')])
+		return ['Not found']
 
 
-def show_environment(environ, start_response):
-	response_body = ['%s: %s\n<br>\n' % (key, value) for key, value in sorted(environ.items())]
-	start_response('200 OK', [('Content-Type', 'text/html; charset=utf-8')])
+	def get_databases(self, profile):
+		c = self.connection_params(profile)
+		dbm = base.DataBaseManager(**c)
+		self.start_response('200 OK', [('Content-Type', 'text/html; charset=utf-8')])
+		return json.JSONEncoder().encode(dbm.get_databases())
 
-	return response_body
+
+	def show_environment(self):
+		response_body = ['%s: %s\n<br>\n' % (key, value) for key, value in sorted(self.environ.items())]
+		self.start_response('200 OK', [('Content-Type', 'text/html; charset=utf-8')])
+
+		return response_body
 
 
-def not_found(environ, start_response):
-	""" 404 """
-	start_response('404 NOT FOUND', [('Content-Type', 'text/plain')])
-	return ['Not found']
+	def index(self):
+		""" Display index page """
+		self.start_response('200 OK', [('Content-Type', 'text/html; charset=utf-8')])
 
-def index(environ, start_response):
-	""" Display index page """
-	start_response('200 OK', [('Content-Type', 'text/html; charset=utf-8')])
+		template = env.get_template('index.html')
 
-	template = env.get_template('index.html')
+		connections = base.Connections()
+		connection_options = list()
+		for name in connections.list_connection():
+			options = connections.get_connection_params(name)
+			options['name'] = name
+			connection_options.append(options)
 
-	connections = base.Connections()
-	connection_options = list()
-	for name in connections.list_connection():
-		options = connections.get_connection_params(name)
-		options['name'] = name
-		connection_options.append(options)
+		software = {
+			'name': SOFTWARE_NAME,
+			'version': SOFTWARE_VERSION,
+		}
+		return template.render(connections=connection_options, message='Hello, world', software=software).encode('utf8')
 
-	software = {
-		'name': SOFTWARE_NAME,
-		'version': SOFTWARE_VERSION,
-	}
-	return template.render(connections=connection_options, message='Hello, world', software=software).encode('utf8')
+	def connection_params(self, profile, dbname = None):
+		dbc = base.Connections().get_connection_params(profile)
+		if dbname is not None:
+			dbc['dbname'] = dbname
+		return dbc
 
-def connection_params(profile, dbname = None):
-	dbc = base.Connections().get_connection_params(profile)
-	if dbname is not None:
-		dbc['dbname'] = dbname
-	return dbc
+	def show_table(self, profile, dbname, schema, table, show):
+		connection = self.connection_params(profile, dbname)
+		dbm = base.DataBaseManager(**connection)
+		self.start_response('200 OK', [('Content-Type', 'text/html; charset=utf-8')])
 
-def show_table(environ, start_response, profile, dbname, schema, table, show):
-	connection = connection_params(profile, dbname)
-	dbm = base.DataBaseManager(**connection)
-	start_response('200 OK', [('Content-Type', 'text/html; charset=utf-8')])
+		if show == 'data':
+			tbl = dbm.get_table_data(schema, table)
+			template = env.get_template('table.data.html')
+		else:
+			tbl = dbm.get_table_structure(schema, table)
+			template = env.get_template('table.structure.html')
 
-	if show == 'data':
-		tbl = dbm.get_table_data(schema, table)
-		template = env.get_template('table.data.html')
-	else:
-		tbl = dbm.get_table_structure(schema, table)
-		template = env.get_template('table.structure.html')
+		types = dbm.get_types()
 
-	types = dbm.get_types()
+		result = template.render(table=tbl, table_info={'table': table, 'dbname': dbname, 'schema': schema}, types=types, connection=connection)
+		return result.encode("utf8")
 
-	result = template.render(table=tbl, table_info={'table': table, 'dbname': dbname, 'schema': schema}, types=types, connection=connection)
-	return result.encode("utf8")
+	def show_schema(self, profile, dbname, schema):
+		dbm = base.DataBaseManager(**self.connection_params(profile, dbname))
+		self.start_response('200 OK', [('Content-Type', 'text/html; charset=utf-8')])
+		ttpl = tpl.Template('templates/b.table.html')
+		for table in dbm.get_tables(schema):
+			ttpl.block({'table_name': table})
+		return ttpl.render().encode("utf8")
 
-def show_schema(environ, start_response, profile, dbname, schema):
-	dbm = base.DataBaseManager(**connection_params(profile, dbname))
-	start_response('200 OK', [('Content-Type', 'text/html; charset=utf-8')])
-	ttpl = tpl.Template('templates/b.table.html')
-	for table in dbm.get_tables(schema):
-		ttpl.block({'table_name': table})
-	return ttpl.render().encode("utf8")
+	def render_schema(self, dbm):
+		stpl = tpl.Template('templates/b.schema.html')
+		for schema in dbm.get_schemas():
+			stpl.block({'schema_name': schema})
+		return stpl.render().encode("utf8")
 
-def render_schema(dbm):
-	stpl = tpl.Template('templates/b.schema.html')
-	for schema in dbm.get_schemas():
-		stpl.block({'schema_name': schema})
-	return stpl.render().encode("utf8")
+	def show_database(self, profile, dbname):
+		dbm = base.DataBaseManager(**self.connection_params(profile, dbname))
+		self.start_response('200 OK', [('Content-Type', 'text/html; charset=utf-8')])
+		return self.render_schema(dbm).encode("utf8")
 
-def show_database(environ, start_response, profile, dbname):
-	dbm = base.DataBaseManager(**connection_params(profile, dbname))
-	start_response('200 OK', [('Content-Type', 'text/html; charset=utf-8')])
-	return render_schema(dbm).encode("utf8")
-
-def dispatch_request(environ, start_response):
-	path = environ.get('PATH_INFO', '').strip('/')
-	for regex, callback in urls:
-		match = re.search(regex, path)
-		if match is not None:
-			environ['pass.args'] = match.groups()
-			return globals()[callback](environ, start_response, **match.groupdict())
-	else:
-		return not_found(environ, start_response)
 
 
